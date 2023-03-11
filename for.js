74210e394d3dd629652c441a2a0f30af546685bb
Cristian Torres <coozaa83@gmail.com>
2023-03-10 21:49:07 -0300

    cambio semantica

--- for.js
+++ for.js
@@ -1,15 +1,15 @@
-//iteradores
+//iteradores  // el For se usa cuando sabemos la cantidad de iteradore que va a tener este ciclo 
 
-// for (inicializacion; condicion; incremento/decre mento)
+// for (inicializacion; condicion; incremento/decremento)
 var x = o;
-for (var x < 5; x++) {       //para incluir 5; podemos usar <=
+for (x; x < 5; x++) {       //para incluir 5; podemos usar <=
     console.log("el valor de x es: " + x); //sentencia 
 
 }                 
 
 
-var y = 1o;
-for (var x < 5; x--) {       //decremento
+var y = 10;
+for (y; x < 5; x--) {       //decremento
     console.log("el valor de x es: " + y); //sentencia 
 
 }                 
---
+++ while.js
@@ -0,0 +1,10 @@
+// el iterador while se usa para cuando no sabemos cuantos iteraciones van haber.
+//por ejemplo se puede usar para cuando traemos registro de una base de datos
+
+var x =10;
+
+while (x >0) {
+    console.log("mi variable vale: "+x);
+    x--;
+
+} 
