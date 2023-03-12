63eb6b7b3e235d057e8a4c9c478d735112ed9245
Cristian Torres <coozaa83@gmail.com>
2023-03-12 10:16:06 -0300

    cabios js

---
+++ sintaxis.js
@@ -0,0 +1,6 @@
+
+// una variable tiene que esta cambiando a lo largo de la ejecucion del programa
+
+var miVariable;
+let nombre;
+const IVA = 21;  //desde que comienza hasta que termina su valor no cambia
